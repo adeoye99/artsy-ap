09a958b7f36e9c13364f7da90f0cb70f248db16c
Sulaimon Adeoye <adeoyesulaimon@yahoo.com>
2023-01-16 12:13:07 +0100

    working on slider

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,8 +1,31 @@
 import React from "react"
- 
+import product1 from "../assets/Images/product1.png"
+import product2 from "../assets/Images/product2.png"
+import product3 from "../assets/Images/product3.png"
 
+const ImageSlider = [
+    {
+      url : product1
+    },
+    {
+       url : product2
 
+    },
+    {
+        url : product3
+    },
+]
 const Slider = () =>{
+    return (
+        <>
+            <h1>
+                THE SLIDER
+            </h1>
+            <div className = "h-full w-full bg-center ">  
+
+            </div>     
+        </>
+    )
 
 
 }
