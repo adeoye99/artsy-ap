f5608f5cc3dd68b83738b253a15189a8d0d4629d
Sulaimon Adeoye <adeoyesulaimon@yahoo.com>
2023-01-16 14:25:34 +0100

    started the slider

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import product1 from "../assets/Images/product1.png"
 import product2 from "../assets/Images/product2.png"
 import product3 from "../assets/Images/product3.png"
+import BackgroundImage from "../assets/Images/backgroundslider.png"
 
 const ImageSlider = [
     {
@@ -18,13 +19,17 @@ const ImageSlider = [
 const Slider = () =>{
     return (
         <>
+            <div style = {{ backgroundImage : `url(${product1})`}} className="hw-full bg-center">
             <h1>
                 THE SLIDER
             </h1>
-            <div className = "h-full w-full bg-center ">  
-
+            <div style = {{ backgroundImage : `url(${ImageSlider[0].url})`}}  className = "h-[80%] w-[80%] m-auto bg-center bg-cover">  
+                  
             </div>     
+          </div>
+        
         </>
+        
     )
 
 
