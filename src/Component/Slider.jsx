4c53a3ca4e401e4d5047ba07b3985ad6ab4ab643
Sulaimon Adeoye <adeoyesulaimon@yahoo.com>
2023-01-17 10:39:21 +0100

    working on the slider

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -3,6 +3,9 @@ import product1 from "../assets/Images/product1.png"
 import product2 from "../assets/Images/product2.png"
 import product3 from "../assets/Images/product3.png"
 import BackgroundImage from "../assets/Images/backgroundslider.png"
+import Backward from "../assets/Images/backwardarrow.png"
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+
 
 const ImageSlider = [
     {
@@ -19,13 +22,28 @@ const ImageSlider = [
 const Slider = () =>{
     return (
         <>
-            <div style = {{ backgroundImage : `url(${product1})`}} className="hw-full bg-center">
-            <h1>
-                THE SLIDER
-            </h1>
-            <div style = {{ backgroundImage : `url(${ImageSlider[0].url})`}}  className = "h-[80%] w-[80%] m-auto bg-center bg-cover">  
+            <div style = {{ backgroundImage : `url(${BackgroundImage})`}} className = "min-h-screen w-full bg-center bg-cover">
+              <div className="w-[80%] mx-auto my-auto">
+                 <div className="w-[80%] align-left">
+                   <h1 className="text-white text-[25px] text-left border-b-1 border-yellow py-2">See Upcoming Aunction and Exhibition</h1>
+                 </div>
+
+                 <div style = {{ backgroundImage : `url(${product1})`}}  className = "h-[500px] w-[100%] mx-auto  bg-center bg-cover opacity-100 mt-2">  
                   
-            </div>     
+                 </div>
+              </div>
+              <div className="absolute bottom-3 right-3 grid grid-cols-2 z-auto">
+                     cnvdkcvjfcfvb
+                    <div style={{backgroundImage :`url(${Backward})`}}>
+                       <MdOutlineArrowForwardIos/> 
+                    </div>
+                    <div style={{backgroundImage :`url(${Backward})`}}>
+                       <MdOutlineArrowForwardIos/> 
+                    </div>
+
+
+                 </div>
+                
           </div>
         
         </>
