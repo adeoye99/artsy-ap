7fa8207e8a7c5577a5b8feb2dbf063aaf8e3da32
Sulaimon Adeoye <adeoyesulaimon@yahoo.com>
2023-01-22 16:03:34 +0100

    working on the subscribe

--- src/Component/Subscribe.jsx
+++ src/Component/Subscribe.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Subscribe = () => {
   return (
     <>
-       <div className='border-2 border-black w-[80%] mx-auto text-center'>
+       <div className=' md:border-2 border-black w-[90%] mx-auto text-center mt-[10%] h-[50%] min-h-[50%] py-[3%]'>
            <h1>NEWSLETTER</h1>
            <p>Subscribe to get daily updates on drops & exciting deals </p>
            <div className=''>
               <input
                 placeholder='ENTER YOUR EMAIL'
               />
-              <button className='bg-black '>
+              <button className='bg-black text-white'>
                   SUBSCRIBE
               </button>
            </div>
