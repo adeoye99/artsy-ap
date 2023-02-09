fcbe3ddc4e616a7bd483f3b854d3879ee285d004
Sulaimon Adeoye <adeoyesulaimon@yahoo.com>
2023-02-09 05:55:12 -0800

    done with the responsiveness of the drop page

--- src/Component/Upcomingdrops.jsx
+++ src/Component/Upcomingdrops.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import upcoming1 from "../assets/Images/upcomingdropimg1.png"
 
-const Upcomingdrops = () => {
+const Upcomingdrops = ({img}) => {
   return (
     <>    
      <div className='md:grid grid-cols-2 w-[80%] mx-auto mt-[10%]' >
-        <div className='bg-center bg-cover h-80 ' style = {{ backgroundImage : `url(${upcoming1})`}}>
-            <div className='grid grid-cols-2 bg-white bg-opacity-25 mx-auto mt-[29%] w-[80%] p-4 rounded-sm'>
+        <div className='bg-center bg-cover h-80 ' style = {{ backgroundImage : `url(${img})`}}>
+            <div className=' md:grid grid-cols-2 bg-white bg-opacity-25 mx-auto md:mt-[35%] w-[80%] p-4 rounded-sm'>
                <div className='flex flex-col text-white text-left pt-[3%] '>
                   <p className='text-sm'>Time renmaining</p>
                   <p className='text-2xl mt-[3%]'>06 hrs : 45 mins : 22s</p>
@@ -14,22 +14,22 @@ const Upcomingdrops = () => {
                </div>
                <div>
                 
-                  <button className='bg-[#4693ED] text-white mt-[20%] mx-auto w-[30%] rounded-[15px]'>Join</button>
+                  <button className='bg-[#4693ED] text-white mt-[20%] mx-auto w-[40%] rounded-[15px]'>Join</button>
 
                </div>
             </div>
         
         </div>
         <div className='text-left'>
-           <div className='md:ml-[50px]'> 
+           <div className='mt-[20px] md:ml-[50px] md:mt-0'> 
               <button className = "border bg-[#4693ED] px-4 py-2 rounded-lg text-white ">Upcoming</button>
               <p>November 21 at 11 am WAT</p>
-              <p className='text-2xl font-bold my-3'>  Eyo : Eko For Show</p>
+              <p className='text-4xl font-bold my-3'>  Eyo : Eko For Show</p>
               <p className='py-4 text-[#616161]'>Lorem ipsum dolor sit amet consectetur. Amet<br/> odio a aenean quis vitae tempus. Sed nunc <br/> tempus aliquet lectus ut vulputate.</p>
               <p className='flex'>
                <p className='p'> Creator </p>  <p>:</p> 
                 
-                <p className='text-[#006CA2]'> Aliya Minat </p>
+                <p className='text-[#006CA2] ml-2'> Aliya Minat </p>
               </p>
               
               <p className='pt-2'>Get notified</p>
--- src/Pages/Drop.jsx
+++ src/Pages/Drop.jsx
@@ -1,30 +1,53 @@
 import React from "react"
 import Upcomingdrops from "../Component/Upcomingdrops"
+import upcoming1 from "../assets/Images/upcomingdropimg1.png"
+import upcoming2 from "../assets/Images/upcomingdropimg2.png"
+import upcoming3 from "../assets/Images/upcomingdropimg3.png"
 
+
+const Upcomingdrop = [
+
+    {
+      img : upcoming1
+
+    },
+    {
+      img : upcoming2
+
+    },
+    {
+      img: upcoming3
+
+    }
+]
 
 const Drop = () =>{
     return (
         <div>
               <div className="h-[300px]">
                 <div className="mx-auto mt-[100px]">
-                  <h1 className="text-3xl font-semibold">Upcoming Drop</h1>
-                  <p className="mt-[2%] text-xl text-gray-300">Turn on notifications so that no drops will miss you.</p>
-                  <button className="w-[50%] border border-black md:border md:border-black rounded-sm md:w-[18%] h-[45px] mt-[1%]" >Notify me</button>
+                  <h1 className="text-5xl font-semibold">Upcoming Drop</h1>
+                  <p className="mt-[2%] text-xl text-gray-500">Turn on notifications so that no drops will miss you.</p>
+                  <button className="w-[50%] border border-black md:border md:border-black rounded-sm md:w-[18%] h-[45px] mt-[1%] hover:bg-opacity-25" >Notify me</button>
                  </div>
               </div>
               <div>
-                 <Upcomingdrops
+                {
+                    Upcomingdrop.map((item)=>{
+                        return(
+                            <Upcomingdrops
+                              img = {item.img}
                    
-                 />
-                 <Upcomingdrops
-                   
-                 />
-                 <Upcomingdrops
-                   
-                 />
-                 <Upcomingdrops
-                   
-                 />
+                             />
+                        )
+
+                    })
+
+
+                } 
+                  
+                 
+             
                 
               </div>
         </div>
