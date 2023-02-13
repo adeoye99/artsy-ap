743efc8d5ba706453162e47a201731cb1826fc71
Adeoye Sulaimon Kolawole <adeoyesulaimon@yahoo.com>
2023-02-13 07:50:14 +0100

    changes in product sample

--- src/Component/ProductSample.jsx
+++ src/Component/ProductSample.jsx
@@ -5,9 +5,9 @@ import ProductS1 from "../assets/Images/ProductS1.png"
 const ProductSample = ({image ,  name ,price}) => {
   return (
     <>
-    <div className='bg-white  h-[370px] text-left float-left ml-[8%] mt-[5%] p-3 rounded-md mb-[5%]'>
+    <div className=' w-[80%] md:w-[20%]  md:bg-white md:h-[370px] text-left float-left ml-[8%] mt-[5%] p-3 rounded-md mb-[5%]'>
        <img 
-         className=' '
+         className='w-[100%] '
           src = {image}
         />
         <p>{name}</p>
