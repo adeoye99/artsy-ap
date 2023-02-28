e2698324132f67ef12c2ccaf4cbdb07c262e215c
Adeoye Sulaimon Kolawole <adeoyesulaimon@yahoo.com>
2023-02-28 22:52:02 +0100

    changes

--- src/Component/ShoppingDetails.jsx
+++ src/Component/ShoppingDetails.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const ShoppingDetails = () => {
   return (
     <div>
-        <h1>ShoppingDetails</h1>
-        <h1>ShoppingDetails</h1>
-       
+        
         
         
     </div>
